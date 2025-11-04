5cdc8d01a11eed3a16a2a3b733a04e139c228e01
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-11-04 09:16:30 +0900

    ✨Refactor: SignupRequest(요청DTO) -> validate 상수로 뺌

--- src/main/java/com/carrot/market/dto/SignupRequest.java
+++ src/main/java/com/carrot/market/dto/SignupRequest.java
@@ -2,6 +2,7 @@ package com.carrot.market.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,8 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignupRequest {
 
+    // Validation 제약 조건 상수
+    private static final int USERNAME_MIN_LENGTH = 4;
+    private static final int USERNAME_MAX_LENGTH = 20;
+    private static final int PASSWORD_MIN_LENGTH = 8;
+    private static final int PASSWORD_MAX_LENGTH = 40;
+    private static final int NICKNAME_MIN_LENGTH = 2;
+    private static final int NICKNAME_MAX_LENGTH = 20;
+    private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
     @NotBlank(message = "사용자명은 필수입니다")
-    @Size(min = 4, max = 20, message = "사용자명은 4~20자여야 합니다")
+    @Size(min = USERNAME_MIN_LENGTH, max = USERNAME_MAX_LENGTH,
+          message = "사용자명은 " + USERNAME_MIN_LENGTH + "~" + USERNAME_MAX_LENGTH + "자여야 합니다")
     private String username;
 
     @NotBlank(message = "이메일은 필수입니다")
@@ -21,11 +32,17 @@ public class SignupRequest {
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다")
-    @Size(min = 6, max = 40, message = "비밀번호는 6~40자여야 합니다")
+    @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH,
+          message = "비밀번호는 " + PASSWORD_MIN_LENGTH + "~" + PASSWORD_MAX_LENGTH + "자여야 합니다")
+    @Pattern(
+            regexp = PASSWORD_PATTERN,
+            message = "비밀번호는 대문자, 소문자, 숫자, 특수문자(@$!%*?&)를 각각 최소 1개 이상 포함해야 합니다"
+    )
     private String password;
 
     @NotBlank(message = "닉네임은 필수입니다")
-    @Size(min = 2, max = 20, message = "닉네임은 2~20자여야 합니다")
+    @Size(min = NICKNAME_MIN_LENGTH, max = NICKNAME_MAX_LENGTH,
+          message = "닉네임은 " + NICKNAME_MIN_LENGTH + "~" + NICKNAME_MAX_LENGTH + "자여야 합니다")
     private String nickname;
 
     private String phoneNumber;
